889edadbf56b510353226636c4ad09070a39c79d
Esther Kang <eskang87@gmail.com>
2022-03-23 14:28:48 -0700

    commit tree 8fa10eec89091d30f72de45fe461302b7eaf68ee

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    //console.log("useEffect Test")
+    const timer = setTimeout(() => {
+      if(timeRemaining > 0){
+        setTimeRemaining(timeRemaining - 1)
+      } else {
+        setTimeRemaining(10)
+        onAnswered(false)
+      }
+    }, 1000)
+
+    return function cleanup() {
+      clearTimeout(timer)
+    }
+
+  }, [timeRemaining, onAnswered])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
